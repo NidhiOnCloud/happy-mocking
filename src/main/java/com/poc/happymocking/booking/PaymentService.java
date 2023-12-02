526496de427e2297850618dee1496b3315a00846
NidhiOnCloud <srinidhi.cloud@gmail.com>
2023-12-02 20:14:27 +0100

    constants for price and guests

--- src/main/java/com/poc/happymocking/booking/PaymentService.java
+++ src/main/java/com/poc/happymocking/booking/PaymentService.java
@@ -3,9 +3,11 @@ package com.poc.happymocking.booking;
 import java.util.*;
 
 public class PaymentService {
+	public static final double MIN_PRICE = 200.0;
+	public static final int MAX_GUESTS = 3;
 	private final Map<String, Double> payments = new HashMap<>();
 	public String pay(BookingRequest bookingRequest, double price) {
-		if (price > 200.0 && bookingRequest.getGuestCount() < 3) {
+		if (price > MIN_PRICE && bookingRequest.getGuestCount() < MAX_GUESTS) {
 			throw new UnsupportedOperationException("Only small payments are supported.");
 		}
 		String id = UUID.randomUUID().toString();
